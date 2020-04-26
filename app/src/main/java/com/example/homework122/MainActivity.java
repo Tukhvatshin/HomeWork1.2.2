4f01664cbc947e8d356007eeb7012b6a4ba687c1
Tukhvatshin <tukhvatshin@gmail.com>
2020-04-26 16:39:34 +0300

    Random плохо реализован, хочется решение в одну строку

--- app/src/main/java/com/example/homework122/MainActivity.java
+++ app/src/main/java/com/example/homework122/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.homework122;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+
+    @SuppressLint("StringFormatMatches")
+    private void init() {
+
+        final TextView my_foto = (TextView) findViewById(R.id.my_foto);
+        final TextView textView_link = (TextView) findViewById(R.id.textView_link);
+
+        Random random = new Random();
+        int i = random.nextInt(1000) + 1;
+        textView_link.setText(getString(R.string.success_subscription, i));
+
+        Button button_forward = findViewById(R.id.button_forward);
+        button_forward.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button button_back = findViewById(R.id.button_back);
+        button_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+                finish();
+            }
+        });
     }
 }
